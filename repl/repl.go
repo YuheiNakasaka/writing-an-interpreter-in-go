60d03e01e12831ce6da27fecd6a04c51286944f7
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2018-06-15 00:04:59 +0900

    REPL interpreted Integer

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/YuheiNakasaka/writing-an-interpreter-in-go/evaluator"
 	"github.com/YuheiNakasaka/writing-an-interpreter-in-go/lexer"
 	"github.com/YuheiNakasaka/writing-an-interpreter-in-go/parser"
 )
@@ -31,8 +32,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
